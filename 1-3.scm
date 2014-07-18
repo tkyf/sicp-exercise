6ffb69f500112619b7cd69dfd39190905224770c
tkyf <tkyf@users.noreply.github.com>
2014-07-18 23:21:39 +0900

    Modified: Defined with square function.

--- 1-3.scm
+++ 1-3.scm
@@ -1,6 +1,12 @@
+#lang racket
+
+(define (square x)
+    (* x x))
+
 (define (max_square x y z)
-     (if (<= x y)
-         (if (<= z x) (+ (* x x) (* y y))
-             (+ (* y y) (* z z)))
-         (if(<= z y) (+ (* x x) (* y y))
-             (+ (* x x) (* z z)))))
+    (if (<= x y)
+        (if (<= z x) (+ (square x) (square y y))
+            (+ (square y) (square z)))
+        (if(<= z y) (+ (square x) (square y))
+            (+ (square x) (square z)))))
+
